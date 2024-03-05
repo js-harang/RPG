e27b681fbf426b90a9b3276fcde2a30f57ba9d00
js-harang <87293890+js-harang@users.noreply.github.com>
2024-03-05 15:02:09 +0900

    2024-03-05

---
+++ RPG/Chapter-01-02/Assets/FastCampus/Jin_Scripts/RigidbodyCharacter.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+    #region Variables
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float dashDistance = 5f;
+
+    private Rigidbody rigidbody;
+
+    private Vector3 inputDirection = Vector3.zero;
+
+    private bool isGrounded = false;
+    public LayerMask groundLayerMask;
+    public float groundCheckDistance = 0.3f;
+    #endregion Variables
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Process user input
+        inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("Horizontal");
+        inputDirection.z = Input.GetAxis("Vertical");
+
+        if (inputDirection != Vector3.zero)
+        {
+            transform.forward = inputDirection;
+        }
+
+        // Process jump input
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+            rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+
+
+        // Process dash input
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward,
+                dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime),
+                0,
+                (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
+            rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);
+    }
+}
